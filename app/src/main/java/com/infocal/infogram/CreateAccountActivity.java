d9c4daeee13c3b8eaeebdde79a616e21ac356896
xXCaptainJhonaXx <fabrizzioachabal070601@gmail.com>
2025-09-08 13:02:14 -0400

    Activity Crear Cuenta

---
+++ app/src/main/java/com/infocal/infogram/CreateAccountActivity.java
@@ -0,0 +1,18 @@
+package com.infocal.infogram;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class CreateAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+    }
+}
