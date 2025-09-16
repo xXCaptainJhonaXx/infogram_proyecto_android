5f263a8279bd6a556ed658e4335433b5afb8f50b
xXCaptainJhonaXx <fabrizzioachabal070601@gmail.com>
2025-09-16 01:11:09 -0400

    Transiciones en Android

--- app/src/main/java/com/infocal/infogram/adapter/CardViewAdapter.java
+++ app/src/main/java/com/infocal/infogram/adapter/CardViewAdapter.java
@@ -2,7 +2,9 @@ package com.infocal.infogram.adapter;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Build;
 import android.text.Layout;
+import android.transition.Explode;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +12,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityOptionsCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.infocal.infogram.R;
@@ -55,7 +58,15 @@ public class CardViewAdapter extends RecyclerView.Adapter<CardViewAdapter.CardVi
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(activity, ImageDetailActivity.class);
-                activity.startActivity(intent);
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    Explode explode = new Explode();
+                    explode.setDuration(1000);
+                    activity.getWindow().setExitTransition(explode);
+
+                    activity.startActivity(intent, ActivityOptionsCompat.makeSceneTransitionAnimation(activity, view, activity.getString(R.string.transitionName_imageCardView)).toBundle());
+                } else {
+                    activity.startActivity(intent);
+                }
             }
         });
 
