847fd6c353bad2e7ebdf5786194d5f20d4ff8617
xXCaptainJhonaXx <fabrizzioachabal070601@gmail.com>
2025-09-15 22:16:45 -0400

    CircleImageView y SimpleViewBehavior

--- app/src/main/java/com/infocal/infogram/view/fragment/ProfileFragment.java
+++ app/src/main/java/com/infocal/infogram/view/fragment/ProfileFragment.java
@@ -2,13 +2,21 @@ package com.infocal.infogram.view.fragment;
 
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.infocal.infogram.R;
+import com.infocal.infogram.adapter.CardViewAdapter;
+import com.infocal.infogram.model.Image;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -61,6 +69,50 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        //toolbar
+        showToolbar("", false, view);
+
+        //recyclerview
+        RecyclerView recyclerView = view.findViewById(R.id.recycle_view_profile);
+
+        //Layout manager
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        //adapter
+
+        CardViewAdapter cardViewAdapter= new CardViewAdapter(buidImages(), R.layout.cardview_image, getActivity());
+        recyclerView.setAdapter(cardViewAdapter);
+        return view;
+    }
+
+    public void showToolbar(String titulo, boolean botonSubir, View view) {
+        Toolbar toolbar = view.findViewById(R.id.toolbar_profile);
+        ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle(titulo);
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(botonSubir);
+
+
+    }
+    //crear lista de imagenes
+
+    public ArrayList<Image> buidImages() {
+        ArrayList<Image> images =new ArrayList<>();
+        images.add(new Image("https://articles-img.sftcdn.net/t_article_cover_xl/auto-mapping-folder/sites/2/2015/04/GTAV-Review.jpg", "Carlos Valdivia", " 2 dias", " 1 me gusta"));
+        images.add(new Image("https://forum-en-cdn.warthunder.com/original/3X/7/a/7a58fcdf6004e4143d53d218ff961c87679b5cad.jpeg", "Luis Rojas", " 7 dias", " 6 me gusta"));
+        images.add(new Image("https://media-cdn.tripadvisor.com/media/attractions-splice-spp-720x480/0a/ae/19/87.jpg", "Johnny Mamani", " 10 dias", " 2 me gusta"));
+        images.add(new Image("https://upload.wikimedia.org/wikipedia/commons/3/30/Shot_Kal-.jpg", "Jose Herrera", " 4 dias", " 9 me gusta"));
+        images.add(new Image("https://upload.wikimedia.org/wikipedia/commons/thumb/0/03/F-104_right_side_view.jpg/1024px-F-104_right_side_view.jpg", "Carlos Moscado", " 3 dias", " 19 me gusta"));
+        images.add(new Image("https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/ERC-90_Sagaie_008_FR.JPG/1280px-ERC-90_Sagaie_008_FR.JPG", "Maria Flores", " 8 dias", " 8 me gusta"));
+        images.add(new Image("https://upload.wikimedia.org/wikipedia/commons/thumb/9/97/Intel_Headquarters_in_2023.jpg/1280px-Intel_Headquarters_in_2023.jpg", "Juan Balderrama", " 7 dias", " 3 me gusta"));
+        images.add(new Image("https://upload.wikimedia.org/wikipedia/commons/thumb/d/db/MiG-21_LanceR_taking_off_Romania_Oct_2009.jpg/1280px-MiG-21_LanceR_taking_off_Romania_Oct_2009.jpg", "Lorena Patzi", " 5 dias", " 4 me gusta"));
+        images.add(new Image("https://upload.wikimedia.org/wikipedia/commons/d/d7/T-55_skos_RB.jpg", "Pedro Valdivia", " 6 dias", " 2 me gusta"));
+        images.add(new Image("https://upload.wikimedia.org/wikipedia/commons/thumb/7/72/Goyas_2025_-_Antonio_Banderas_%28cropped%29.jpg/800px-Goyas_2025_-_Antonio_Banderas_%28cropped%29.jpg", "Antonio Banderas", " 1 dias", " 3 me gusta"));
+
+        return images;
+
     }
 }
